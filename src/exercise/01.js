4fcf76f365cbaa5cc5c8076f346776370540cf67
jvogt2306 <jan_vogt92@web.de>
2022-10-14 17:08:39 +0200

    :sparkles: useReducer (increment etc.) and useCallback for async functions

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,40 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+const initState = {count: 0}
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    case 'DECREMENT': {
+      return {count: state.count - action.step}
+    }
+    case 'RESET': {
+      return initState
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
+  }
+}
+
+function Counter({step = 1}) {
+  const [state, dispatch] = React.useReducer(countReducer, initState)
+  const {count} = state
+  const incrementDispatch = () => dispatch({type: 'INCREMENT', step})
+  const decrementDispatch = () => dispatch({type: 'DECREMENT', step})
+  const resetDispatch = () => dispatch({type: 'RESET'})
+
+  return (
+    <div>
+      <button onClick={incrementDispatch}>+</button>
+      <span>{count}</span>
+      <button onClick={decrementDispatch}>-</button>
+      <button onClick={resetDispatch}>Reset</button>
+    </div>
+  )
 }
 
 function App() {
